2ed1d5c19e23de075720abbb199300506f520aa1
Edson Junior <edson_junior91@hotmail.com>
2020-09-15 00:00:54 -0300

    Implementação Classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,22 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Client cli = new Client("Maria", "maria@gmail.com",sdf.parse("22/10/1980"));
+		
+		System.out.println(cli);
 	}
 
 }
