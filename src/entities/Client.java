20c1ae63517e574f44b67381b04cf5faab12617e
Edson Junior <edson_junior91@hotmail.com>
2020-09-15 00:01:48 -0300

    Implementação Classe Client, adicionado metodo toString

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -1,8 +1,10 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	private String name;
 	private String email;
 	private Date birthDate;
@@ -32,5 +34,9 @@ public class Client {
 	}
 	public Date getBirthDate() {
 		return birthDate;
+	}
+	@Override
+	public String toString() {
+		return "Client: " + name + " (" + sdf.format(birthDate) + ") - " + email;
 	}
 }
